8614e887336fe70ee33f223816d83a0b9de2c4e4
Konstantin Basmanov <basmanovk@gmail.com>
2020-06-23 16:21:25 -0500

    fixed overflow

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import BackgroundSwitch from './BackgroundSwitch'
 
 function App() {
   let [darkMode, setBackgroundMode] = useState(false)
-
   const handleInputChange = e => {
     setBackgroundMode(e.target.checked)
+    // e.target.checked
+    //   ? document.body.classList.add('darkMode')
+    //   : document.body.classList.remove('darkMode')
   }
   return (
-    <div className={`App ${darkMode ? 'darkMode' : ''}`}>
+    <div className={`App ${darkMode ? 'darkBg' : ''}`}>
       <BackgroundSwitch
         darkMode={darkMode}
         onInputChange={handleInputChange}
@@ -18,42 +20,59 @@ function App() {
         <h1>Hello,</h1>
         <h2 className='intro'>
           I’m <span className='bold'>Konstantin Basmanov</span>, a front-end
-          software engineer with a passion for building captivating interfaces
-          for mobile and desktop devices.
+          software engineer based in Chicago, IL with a passion for building
+          captivating interfaces for mobile and desktop devices.
         </h2>
         <h2 className='intro bold'>Let’s build something together!</h2>
         <br />
         <h3>
           <span>Get in touch </span>
-          <a href='mailto:basmanovk@gmail.com'>basmanovk@gmail.com</a>
+          <a
+            href='mailto:basmanovk@gmail.com'
+            className={darkMode ? 'darkLink' : ''}
+          >
+            basmanovk@gmail.com
+          </a>
         </h3>
       </header>
-      <section className='container'>
-        <h2 className='section-title'>Background</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          perferendis explicabo deserunt expedita, dolor sit soluta vitae
-          perspiciatis id rem velit, aliquid ducimus quisquam libero cupiditate
-          dicta, officia illo culpa.
-        </p>
+      <section className='container flex'>
+        <h2 className={`section-title ${darkMode ? 'darkTitle' : ''}`}>
+          Background
+        </h2>
+        <div className='section-content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            perferendis explicabo deserunt expedita, dolor sit soluta vitae
+            perspiciatis id rem velit, aliquid ducimus quisquam libero
+            cupiditate dicta, officia illo culpa.
+          </p>
+        </div>
       </section>
-      <section className='container'>
-        <h2 className='section-title'>Skills</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          perferendis explicabo deserunt expedita, dolor sit soluta vitae
-          perspiciatis id rem velit, aliquid ducimus quisquam libero cupiditate
-          dicta, officia illo culpa.
-        </p>
+      <section className='container flex'>
+        <h2 className={`section-title ${darkMode ? 'darkTitle' : ''}`}>
+          Skills
+        </h2>
+        <div className='section-content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            perferendis explicabo deserunt expedita, dolor sit soluta vitae
+            perspiciatis id rem velit, aliquid ducimus quisquam libero
+            cupiditate dicta, officia illo culpa.
+          </p>
+        </div>
       </section>
-      <section className='container'>
-        <h2 className='section-title'>Featured Projects</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          perferendis explicabo deserunt expedita, dolor sit soluta vitae
-          perspiciatis id rem velit, aliquid ducimus quisquam libero cupiditate
-          dicta, officia illo culpa.
-        </p>
+      <section className='container flex'>
+        <h2 className={`section-title ${darkMode ? 'darkTitle' : ''}`}>
+          Featured Projects
+        </h2>
+        <div className='section-content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            perferendis explicabo deserunt expedita, dolor sit soluta vitae
+            perspiciatis id rem velit, aliquid ducimus quisquam libero
+            cupiditate dicta, officia illo culpa.
+          </p>
+        </div>
       </section>
       <footer>
         <h3>
--- src/components/BackgroundSwitch.js
+++ src/components/BackgroundSwitch.js
@@ -6,20 +6,17 @@ export default function BackgroundSwitch({
   count,
   onCount
 }) {
-  console.log(darkMode)
   return (
     <div className='switch-container'>
-      <form>
-        <label className='switch'>
-          <input
-            name='switch'
-            type='checkbox'
-            checked={darkMode}
-            onChange={e => onInputChange(e)}
-          />
-          <span className='slider'></span>
-        </label>
-      </form>
+      <label className='switch'>
+        <input
+          name='switch'
+          type='checkbox'
+          checked={darkMode}
+          onChange={e => onInputChange(e)}
+        />
+        <span className='slider'></span>
+      </label>
     </div>
   )
 }

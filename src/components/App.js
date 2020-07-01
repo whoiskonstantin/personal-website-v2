1d104fd80f3c89deda859ef6d4f161b9360401d6
Konstantin Basmanov <basmanovk@gmail.com>
2020-06-30 19:50:26 -0500

    footer complete

--- src/components/App.js
+++ src/components/App.js
@@ -17,13 +17,10 @@ function App() {
     return false
   }
 
-  // let [darkMode, setBackgroundMode] = useState(handleBackgorundColor())
-  let [darkMode, setBackgroundMode] = useState(true)
+  let [darkMode, setBackgroundMode] = useState(handleBackgorundColor())
+
   const handleInputChange = e => {
     setBackgroundMode(e.target.checked)
-    // e.target.checked
-    //   ? document.body.classList.add('darkMode')
-    //   : document.body.classList.remove('darkMode')
   }
 
   return (
@@ -45,7 +42,7 @@ function App() {
           <span>Get in touch </span>
           <a
             href='mailto:basmanovk@gmail.com'
-            className={darkMode ? 'darkLink' : ''}
+            className={`contact-link ${darkMode ? 'darkLink' : ''}`}
           >
             basmanovk@gmail.com
           </a>
@@ -148,12 +145,20 @@ function App() {
               Firstline Properties
             </h3>
             <div className='project-images'>
-              <img
-                className='desktop'
-                src={FLdesktop}
-                alt='Firstline website'
-              />
-              <img className='mobile' src={FLmobile} alt='Firstline website' />
+              <a href='https://firstlinebg.com/'>
+                <img
+                  className='desktop'
+                  src={FLdesktop}
+                  alt='Firstline website'
+                />
+              </a>
+              <a href='https://firstlinebg.com/'>
+                <img
+                  className='mobile'
+                  src={FLmobile}
+                  alt='Firstline website'
+                />
+              </a>
             </div>
             <div>
               <p className='project-description'>
@@ -176,16 +181,20 @@ function App() {
               Travel Hangman
             </h3>
             <div className='project-images'>
-              <img
-                className='mobile mobile-large'
-                src={Hangman1}
-                alt='Personal Website v1'
-              />
-              <img
-                className='mobile mobile-large'
-                src={Hangman2}
-                alt='Personal Website v1'
-              />
+              <a href='https://hangman.konstantinbasmanov.com/'>
+                <img
+                  className='mobile mobile-large'
+                  src={Hangman1}
+                  alt='Personal Website v1'
+                />
+              </a>
+              <a href='https://hangman.konstantinbasmanov.com/'>
+                <img
+                  className='mobile mobile-large'
+                  src={Hangman2}
+                  alt='Personal Website v1'
+                />
+              </a>
             </div>
             <div>
               <p className='project-description'>
@@ -194,10 +203,15 @@ function App() {
                 choose between different game modes, which include all countries
                 of the world (245), countries recognized by united nations (193)
                 and different regions. Hangman animations were implemented
-                trough GSAP library. Application uses stored JSON file from
-                RESTful API (https://restcountries.eu/rest/v2/all), because of
-                the required multiple computations that would’ve slowed down the
-                rendering.
+                trough GSAP library. Application uses stored{' '}
+                <a
+                  className={`contact-link ${darkMode ? 'darkLink' : ''}`}
+                  href='https://restcountries.eu/rest/v2/all'
+                >
+                  JSON file from RESTful API
+                </a>
+                , because of the required multiple computations that would’ve
+                slowed down the rendering.
               </p>
             </div>
           </div>
@@ -209,34 +223,64 @@ function App() {
               Personal Website v1
             </h3>
             <div className='project-images'>
-              <img
-                className='desktop'
-                src={PWdesktop}
-                alt='Personal Website v1'
-              />
-              <img
-                className='mobile'
-                src={PWmobile}
-                alt='Personal Website v1'
-              />
+              <a href='https://konstantinbasmanov.com/personal-website-v1/'>
+                <img
+                  className='desktop'
+                  src={PWdesktop}
+                  alt='Personal Website v1'
+                />
+              </a>
+              <a href='https://konstantinbasmanov.com/personal-website-v1/'>
+                <img
+                  className='mobile'
+                  src={PWmobile}
+                  alt='Personal Website v1'
+                />
+              </a>
             </div>
             <div>
               <p className='project-description'>
                 My first personal website was also the first website that I
                 built by myself from scratch using HTML5 and CSS3. I made sure
-                that the website is compatible with mobile devices by
-                implementing CSS flex box layout. My web development skills have
-                improved tremendously since that project.
+                that the website is rendered perfectly on both mobile and
+                desktop devices by using responsive design techniques. My web
+                development skills have improved tremendously since that
+                project.
               </p>
             </div>
           </div>
         </div>
       </section>
       <footer>
-        <h3 className='footer'>Designed & Developed by</h3>
-        <h3 className='footer'>
-          Konstantin Basmanov {new Date().getFullYear()}
-        </h3>
+        <ul className='footer contact-list'>
+          <li>
+            <a
+              className={`contact-link ${darkMode ? 'darkLink' : ''}`}
+              href='mailto:basmanovk@gmail.com'
+            >
+              email
+            </a>
+          </li>
+          <li>
+            <a
+              className={`contact-link ${darkMode ? 'darkLink' : ''}`}
+              href='https://github.com/whoiskonstantin'
+            >
+              github
+            </a>
+          </li>
+          <li>
+            <a
+              className={`contact-link ${darkMode ? 'darkLink' : ''}`}
+              href='https://www.linkedin.com/in/basmanov/'
+            >
+              linkedin
+            </a>
+          </li>
+        </ul>
+        <p className='footer'>
+          Created by Konstantin Basmanov {new Date().getFullYear()}
+        </p>
       </footer>
     </div>
   )
